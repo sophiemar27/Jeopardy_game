55157d980d09cd9260cc0512931b0c8e56943732
Abel <abeltareke@Abels-MacBook-Pro.local>
2020-04-10 11:20:42 -0400

    adding questins

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const htmlCatagory = [
-    'What is HTML?',
+    'What is HTML stands for?',
     'What are tags?',
     'How do you insert a comment in HTML?',
     'How do you insert a copyright symbol on a browser page?',
@@ -11,27 +11,33 @@ const jsCatagory = [];
 
 const comandLineCatagory = [];
 
+function stylingTheBody(){
+    document.body.style.backgroundColor = 'blue';
+    document.body.style.color = 'white';
+    document.body.style.fontSize = 'xx-Large';
+}
 
 //for(let i = 0; i < 4; i++){
     let catagory = document.createElement('div');
-    let node = document.createTextNode('HTML')
+    let html = document.createTextNode('HTML')
     catagory.classList = 'catagory'
-    catagory.appendChild(node)
+    catagory.appendChild(html)
     document.body.appendChild(catagory);
     
     console.log(catagory);
 //}
 
 let box = document.createElement('div');
-    let node2 = document.createTextNode('100')
+    let htmlPrizeA = document.createTextNode('100')
     box.classList = 'boxes'
-    box.appendChild(node2)
+    box.appendChild(htmlPrizeA)
     document.body.appendChild(box);
     
-    //console.log(box);
+    console.log(box);
     
-
     box.addEventListener('click', () => {
+        box.disabled = true;
         document.body.textContent = htmlCatagory[0]
-    
-        })
+
+        stylingTheBody()
+    })
